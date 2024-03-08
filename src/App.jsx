aba992785b66e931772d24b234338adcf64e0353
Luis Henrique <luique16@outlook.com>
2024-03-07 20:41:01 -0400

    fst commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1 className="text-4x1 text-center text-violet-700 bg-pink-400/50 p-2 m-3">Portfolio</h1>
+      </div>
+    </>
+
+  )
+}
+
+export default App
