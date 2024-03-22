07e65ed6318a2206664f92f5526790111cba57a0
Luis Henrique <luique16@outlook.com>
2024-03-21 22:00:04 -0400

    editando topbar e todos os temas

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,14 @@
-import React from "react"
-import { ThemeChanger } from "./components/ThemeChanger"
-import { Avatar } from "./components/Avatar"
+import React from "react";
+import { Avatar } from "./components/Avatar";
+import { NavbarTop } from "./components/NavbarTop";
 
 function App() {
-
   return (
     <>
-      <section className="flex justify-center items-center gap-4 flex-col mt-4">
-        <Avatar />
-        <div className="text-2x1">@Spiguerfild</div>
-        <ThemeChanger />
-      </section>
-
-
+      <NavbarTop />
+      <Avatar />
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 
 export const Avatar = () => {
   return (
-
     <div className="avatar">
-      <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+      <div className="w-24 mask mask-hexagon">
         <img src="https://avatars.githubusercontent.com/u/94618960?v=4" />
       </div>
     </div>
-
   );
 }
