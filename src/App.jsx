aefde610340f447114b3338893865a374ca02a62
Luis Henrique <luique16@outlook.com>
2024-03-26 20:27:58 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import { Avatar } from "./components/Avatar";
 import { NavbarTop } from "./components/NavbarTop";
-import img from './assets/splash.png'
+
 function App() {
   return (
     <>
       <NavbarTop />
-      <div className=" shadow-xl image-full">
-        <img src={img} className="w-1/4" />
+
+      <div className="card lg:card-side bg-base-100 shadow-xl">
+        {/* <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure> */}
+        <Avatar />
+
+        <div className="card-body">
+          <h2 className="card-title">New album is released!</h2>
+          <p>Click the button to listen on Spotiwhy app.</p>
+          <div className="card-actions justify-end">
+            <button className="btn btn-primary">Listen</button>
+          </div>
+        </div>
       </div>
-      <Avatar />
+
     </>
   );
 }
--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Avatar = () => {
   return (
     <div className="avatar">
-      <div className="w-24 mask mask-hexagon">
+      <div className="w-72 mask mask-hexagon">
         <img src="https://avatars.githubusercontent.com/u/94618960?v=4" />
       </div>
     </div>
