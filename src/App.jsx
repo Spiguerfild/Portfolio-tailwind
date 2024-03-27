25cdd3ed9b166cf7f0f4cf33288b66751caa7612
Luis Henrique <luique16@outlook.com>
2024-03-26 21:20:58 -0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,15 @@ function App() {
     <>
       <NavbarTop />
 
-      <div className="card lg:card-side bg-base-100 shadow-xl">
-        {/* <figure><img src="https://daisyui.com/images/stock/photo-1494232410401-ad00d5433cfa.jpg" alt="Album" /></figure> */}
-        <Avatar />
-
-        <div className="card-body">
-          <h2 className="card-title">New album is released!</h2>
-          <p>Click the button to listen on Spotiwhy app.</p>
-          <div className="card-actions justify-end">
-            <button className="btn btn-primary">Listen</button>
-          </div>
+      <div className="card flex flex-row  shadow-xl">
+        <div className="w-1/2">
+          <Avatar />
         </div>
+
+        <div className="w-1/2">
+          <Avatar />
+        </div>
+
       </div>
 
     </>
