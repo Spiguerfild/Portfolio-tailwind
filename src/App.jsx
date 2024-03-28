69e0194569cd9cb1498abfdc040f6956cef57d24
Spiguerfild <luique16@outlook.com>
2024-03-28 14:01:55 -0400

    estruturando texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
 import React from "react";
 import { Avatar } from "./components/Avatar";
 import { NavbarTop } from "./components/NavbarTop";
-
+import './App.css'
+import { Theme } from "react-daisyui";
+console.log(Theme)
 function App() {
   return (
-    <>
+    <div>
       <NavbarTop />
+      <div className="px-16">
 
-      <div className="card flex flex-row  shadow-xl">
+        <div className="card flex flex-row mt-20  shadow-xl">
 
-        <div className="w-1/2 mt-96 text-left">
-          <div className="bg-gray-200 p-4 rounded-lg">
-            <p className="text-lg font-semibold text-blue-700">Olá, eu sou o Seu Nome</p>
-            <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tristique tristique lorem, sed fringilla velit volutpat eget. Nulla nec massa velit. Fusce id erat auctor, viverra nisi vitae, vestibulum justo.</p>
+          <div className="w-2/3  text-left">
+            <div className=" p-4 rounded-lg">
+              <p className="text-2xl font-bold text-primary font-inter leading-10">Olá, me chamo Luis Henrique</p>
+              <p className={`text-5xl font-bold  font-inter leading-custom ${Theme === 'forest' ? 'text-red-600' : 'text-green-500'}`}>Front-end Developer |</p>
+              <p className="">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tristique tristique lorem, sed fringilla velit volutpat eget. Nulla nec massa velit. Fusce id erat auctor, viverra nisi vitae, vestibulum justo.</p>
+            </div>
           </div>
-        </div>
 
-        <div className="w-1/2 text-center">
-          <Avatar />
-        </div>
+          <div className=" w-1/3 text-right">
+            <Avatar />
+          </div>
 
+        </div>
       </div>
-
-    </>
+    </div>
   );
 }
 
--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 export const Avatar = () => {
   return (
     <div className="avatar">
-      <div className="w-72 mask mask-squircle">
+      <div className="w-52 mask mask-squircle">
         <img src="https://avatars.githubusercontent.com/u/94618960?v=4" />
       </div>
     </div>
